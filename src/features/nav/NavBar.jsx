da417a4112076bf96f359f7f305b0b2fb2b98e23
Beema Rahim <beemarahim21@gmail.com>
2021-10-27 11:03:04 +0530

    props, useState, Hooks

---
+++ src/features/nav/NavBar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Button, Container, Menu } from "semantic-ui-react";
+
+function NavBar({setFormOpen}) {
+  return (
+    <div>
+      <Menu inverted fixed='top'>
+        <Container>
+          <Menu.Item header>
+            <img src='/assets/logo.png' alt='logo' style={{marginRight:'15px'}} />
+            Re-vents
+          </Menu.Item>
+
+          <Menu.Item name='Events' />
+          <Menu.Item>
+            <Button onClick={()=>setFormOpen(true)} positive inverted content='Create Event' />
+          </Menu.Item>
+
+          <Menu.Item position='right'>
+            <Button basic inverted content='Login' />
+            <Button
+              basic
+              inverted
+              content='Register'
+              style={{ marginLeft: "0.5em" }}
+            />
+          </Menu.Item>
+        </Container>
+      </Menu>
+    </div>
+  );
+}
+
+export default NavBar;
